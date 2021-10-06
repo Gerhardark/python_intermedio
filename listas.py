a006f0200e2707bc062a30153fe83bdf481a9578
Gerhardark <gerhard.reinike@gmail.com>
2021-10-06 03:38:39 -0300

    versión 0.1

--- listas.py
+++ listas.py
@@ -1,5 +1,22 @@
-def run ():
-    print("hola")
+def main ():
+    my_list = [1, 'hello', True, 4.5]
+    my_dict = {'firstname': 'gerhard', 'lastname': 'reinike'}
+
+    super_list = [
+        {'firstname': 'gerhard', 'lastname': 'reinike'},
+        {'firstname': 'hans', 'lastname': 'reinike'},
+        {'firstname': 'karime', 'lastname': 'gonzalez'},
+        {'firstname': 'isabella', 'lastname': 'reinike'},
+    ]
+
+    super_dict = {
+        'natulral': [1,2,3,4,5],
+        'integer_num': [-1,-2,-3,0],
+        'floating_nums': [3.4,3.5,7.0,10.5]
+    }
+
+    for key, value in super_dict.items():
+        print(key, " ", value, " ")
 
 if __name__ == "__main__":
-    run()
+    main()

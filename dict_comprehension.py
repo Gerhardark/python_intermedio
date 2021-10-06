db8f79e801ab863eb5f2e4c0ae72180aa799ac70
Gerhardark <gerhard.reinike@gmail.com>
2021-10-06 04:35:29 -0300

    version 0.4.1

--- dict_comprehension.py
+++ dict_comprehension.py
@@ -1,5 +1,7 @@
+import math 
+
 def main():
-    my_dict = {"numero: " + str(i) : round(i**3) for i in range (1,101) if i % 3 != 0}
+    my_dict = {'raiz cuadrada de ' + str(i) : round(math.sqrt(i),2) for i in range (1,1001)}
     print(my_dict)
 
 if __name__ == '__main__':

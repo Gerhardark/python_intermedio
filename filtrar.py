7f2c7ecf0700afb55c175ddae7f1dbc05d2255f2
Gerhardark <gerhard.reinike@gmail.com>
2021-10-06 21:33:01 -0300

    version 0.6.1

--- filtrar.py
+++ filtrar.py
@@ -1,6 +1,83 @@
-def main():
-    pass
+DATA = [
+    {
+        'name': 'Facundo',
+        'age': 72,
+        'organization': 'Platzi',
+        'position': 'Technical Coach',
+        'language': 'python',
+    },
+    {
+        'name': 'Luisana',
+        'age': 33,
+        'organization': 'Globant',
+        'position': 'UX Designer',
+        'language': 'javascript',
+    },
+    {
+        'name': 'Héctor',
+        'age': 19,
+        'organization': 'Platzi',
+        'position': 'Associate',
+        'language': 'ruby',
+    },
+    {
+        'name': 'Gabriel',
+        'age': 20,
+        'organization': 'Platzi',
+        'position': 'Associate',
+        'language': 'javascript',
+    },
+    {
+        'name': 'Isabella',
+        'age': 30,
+        'organization': 'Platzi',
+        'position': 'QA Manager',
+        'language': 'java',
+    },
+    {
+        'name': 'Karo',
+        'age': 23,
+        'organization': 'Everis',
+        'position': 'Backend Developer',
+        'language': 'python',
+    },
+    {
+        'name': 'Ariel',
+        'age': 32,
+        'organization': 'Rappi',
+        'position': 'Support',
+        'language': '',
+    },
+    {
+        'name': 'Juan',
+        'age': 17,
+        'organization': '',
+        'position': 'Student',
+        'language': 'go',
+    },
+    {
+        'name': 'Pablo',
+        'age': 32,
+        'organization': 'Master',
+        'position': 'Human Resources Manager',
+        'language': 'python',
+    },
+    {
+        'name': 'Lorena',
+        'age': 56,
+        'organization': 'Python Organization',
+        'position': 'Language Maker',
+        'language': 'python',
+    },
+]
 
+def main():
+    #ll_python_devs = [worker ["name"] for worker in DATA if worker["language"] == "python"]
+    #all_devs_25 = [worker ["name"] for worker in DATA if worker["age"] >= 25]
+    all_devs_adults = list(filter(lambda job: job["age"] >= 25, DATA))
+    all_devs_adults = list(map(lambda job : job["name"], all_devs_adults))
+    for job in all_devs_adults:
+        print(job)
 
 if __name__ == '__main__':
     main()
